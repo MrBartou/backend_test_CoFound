3d9cae11bafb7986f27b071826e86db92d78e2c7
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-05-22 08:53:12 +0200

    feat: add module + change Global

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,19 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationExceptionFilter } from './common/filters/validation-exception.filter';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
-import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { TransformInterceptor } from './common/interceptors/tranform.interceptor';
 import { ErrorsInterceptor } from './common/interceptors/errors.interceptor';
 import { ValidationPipe } from '@nestjs/common';
-import { AuthGuard } from './common/guards/auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const reflector = app.get(Reflector);
-
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalFilters(new ValidationExceptionFilter());
   app.useGlobalFilters(new AllExceptionsFilter());
-
-  app.useGlobalGuards(new JwtAuthGuard());
-  app.useGlobalGuards(new AuthGuard(reflector));
 
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalInterceptors(new TransformInterceptor());
