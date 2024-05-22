d8a76d644d6f208d2df15a018977de7cb1ca829e
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-05-22 13:15:07 +0200

    fix: change url link

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -6,7 +6,7 @@ export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
   async sendUserConfirmation(user: any, token: string) {
-    const url = `example.com/auth/confirm?token=${token}`;
+    const url = `http://localhost:3000/auth/confirm-email?token=${token}`;
 
     await this.mailerService.sendMail({
       to: user.email,
@@ -20,7 +20,7 @@ export class MailService {
   }
 
   async sendPasswordReset(user: any, token: string) {
-    const url = `example.com/auth/reset?token=${token}`;
+    const url = `http://localhost:3000/auth/reset-password?token=${token}`;
 
     await this.mailerService.sendMail({
       to: user.email,
