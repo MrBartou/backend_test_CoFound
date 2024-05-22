dbde0ef22deed8bcac0462cf843806c8a66a1f54
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-05-22 08:58:55 +0200

    feat: add user entities

---
+++ src/modules/user/entities/user.entities.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  userId: number;
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  passwordHash: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  lastLogin: Date;
+
+  @Column({ default: false })
+  isActive: boolean;
+}
