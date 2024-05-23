9ac1cce5acfdabbcd7e6850a9b56335e97e86413
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-05-23 15:42:33 +0200

    feat: add prod config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,11 +5,7 @@ dotenv.config();
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: [__dirname + '/../**/*.entities.{js,ts}'],
+  url: process.env.DATABASE_URL,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
--- src/config/mail.config.ts
+++ src/config/mail.config.ts
@@ -1,17 +1,18 @@
 import { MailerOptions } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export const mailConfig: MailerOptions = {
   transport: {
-    host: process.env.SMTP_HOST || 'mailhog',
-    port: parseInt(process.env.SMTP_PORT, 10) || 1025,
+    host: process.env.MAILGUN_SMTP_SERVER,
+    port: parseInt(process.env.MAILGUN_SMTP_PORT, 10),
+    auth: {
+      user: process.env.MAILGUN_SMTP_LOGIN,
+      pass: process.env.MAILGUN_SMTP_PASSWORD,
+    },
     secure: false,
   },
   defaults: {
-    from: '"No Reply" <noreply@example.com>',
+    from: '"No Reply" <noreply@cofound.com>',
   },
   template: {
     dir: __dirname + '/../modules/mail/templates/',
