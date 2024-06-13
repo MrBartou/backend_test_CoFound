301da59873cae5d5e346cf17ea0c617b2047a028
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-06-13 10:12:56 +0200

    feat: add message module

---
+++ src/modules/message/message.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Message } from './entities/message.entities';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+
+@Injectable()
+export class MessageService {
+  constructor(
+    @InjectRepository(Message)
+    private readonly messageRepository: Repository<Message>,
+  ) {}
+
+  async create(createMessageDto: CreateMessageDto): Promise<Message> {
+    const message = this.messageRepository.create(createMessageDto);
+    return this.messageRepository.save(message);
+  }
+
+  async findAll(): Promise<Message[]> {
+    return this.messageRepository.find({
+      relations: ['conversation', 'fromUser', 'toUser'],
+    });
+  }
+
+  async findOne(id: number): Promise<Message> {
+    return this.messageRepository.findOne({
+      where: { messageId: id },
+      relations: ['conversation', 'fromUser', 'toUser'],
+    });
+  }
+
+  async update(
+    id: number,
+    updateMessageDto: UpdateMessageDto,
+  ): Promise<Message> {
+    await this.messageRepository.update(id, updateMessageDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.messageRepository.delete(id);
+  }
+
+  async read(id: number): Promise<void> {
+    const message = await this.findOne(id);
+    message.read = true;
+    message.readAt = new Date();
+    await this.messageRepository.save(message);
+  }
+}
