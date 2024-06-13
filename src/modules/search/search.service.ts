d5c42f32432fa8c73d0aad337264c6e5f11050c9
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-06-13 16:08:10 +0200

    feat: add search module

---
+++ src/modules/search/search.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ILike, Repository } from 'typeorm';
+import { User } from '../user/entities/user.entities';
+import { Profile } from '../profile/entities/profile.entities';
+import { Project } from '../project/entities/project.entities';
+import { Message } from '../message/entities/message.entities';
+
+@Injectable()
+export class SearchService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Profile)
+    private readonly profileRepository: Repository<Profile>,
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+    @InjectRepository(Message)
+    private readonly messageRepository: Repository<Message>,
+  ) {}
+
+  async searchUsers(query: string): Promise<User[]> {
+    const users = await this.userRepository.find({
+      where: { username: ILike(`%${query}%`) },
+    });
+    return users;
+  }
+
+  async searchProfiles(query: string): Promise<Profile[]> {
+    const profiles = await this.profileRepository.find({
+      where: { skills: ILike(`%${query}%`) },
+    });
+    return profiles;
+  }
+
+  async searchProjects(query: string): Promise<Project[]> {
+    const projects = await this.projectRepository.find({
+      where: { title: ILike(`%${query}%`) },
+    });
+    return projects;
+  }
+
+  async searchMessages(query: string): Promise<Message[]> {
+    const messages = await this.messageRepository.find({
+      where: { messageText: ILike(`%${query}%`) },
+    });
+    return messages;
+  }
+}
